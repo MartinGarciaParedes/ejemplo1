6bcd2befbbf9938aa537ae96defd1c28c448f216
Martín Garcia <martina.garcia.paredes@institutocomercialosorno.cl>
2024-04-03 13:33:20 -0400

    Primer commit

---
+++ script.js
@@ -0,0 +1,15 @@
+function aumentarlike(){
+    var contador = document.getElementById("likes");
+    var likes = parseInt(contador.innerHTML);
+    contador.innerHTML = likes + 1 + "Like(s)";
+}
+function aumentarlike2(){
+    var contador2 = document.getElementById("likes2");
+    var likes2 = parseInt(contador2.innerHTML);
+    contador2.innerHTML = likes2 + 1 + "Like(s)";
+}
+function aumentarlike3(){
+    var contador3 = document.getElementById("likes3");
+    var likes3 = parseInt(contador3.innerHTML);
+    contador3.innerHTML = likes3 + 1 + "Like(s)";
+}
