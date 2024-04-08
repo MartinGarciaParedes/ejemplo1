cd5faddbc8047ed18edc07961e29c3eb7c3de9c6
unknown <martina.garcia.paredes@institutocomercialosorno.cl>
2024-04-08 11:15:42 -0400

    Primer commit

--- script.js
+++ script.js
@@ -1,15 +1,6 @@
-function aumentarlike(){
-    var contador = document.getElementById("likes");
-    var likes = parseInt(contador.innerHTML);
-    contador.innerHTML = likes + 1 + "Like(s)";
-}
-function aumentarlike2(){
-    var contador2 = document.getElementById("likes2");
-    var likes2 = parseInt(contador2.innerHTML);
-    contador2.innerHTML = likes2 + 1 + "Like(s)";
-}
-function aumentarlike3(){
-    var contador3 = document.getElementById("likes3");
-    var likes3 = parseInt(contador3.innerHTML);
-    contador3.innerHTML = likes3 + 1 + "Like(s)";
+function aumentarLike(id) {
+    const likesElement = document.getElementById(id);
+    let currentLikes = parseInt(likesElement.textContent);
+    currentLikes++;
+    likesElement.textContent = currentLikes + (currentLikes === 1 ? 'like' : 'likes');
 }
